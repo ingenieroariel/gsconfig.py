e47fa305a93aaff80ac91f37e42628ae0879051d
David Winslow <dwinslow@opengeo.org>
2010-03-18 10:59:31 -0400

    Correct dependency declaration in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name = "GSConfig",
     url = "http://bitbucket.org/dwins/gsconfigpy",
     author = "David Winslow, Sebastian Benthall",
     author_email = "dwinslow@opengeo.org",
-    requires = ['httplib2'],
+    install_requires = ['httplib2'],
     package_dir = {'':'src'},
     test_suite = "test.catalogtests"
 ) 

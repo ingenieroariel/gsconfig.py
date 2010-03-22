f30ae60ab7abf61bd294533f2c9fbac350f033dc
Ivan Willig <iwillig@gmail.com>
2010-03-22 16:36:03 -0400

    basic support for layergroups

--- src/geoserver/layergroup.py
+++ src/geoserver/layergroup.py
@@ -7,24 +7,33 @@ class LayerGroup(object):
     """
     def __init__(self,catalog,name):
         self.catalog = catalog
-        self.name = name 
+        self.name = name
+        self.xmlNode = get_xml("%s/layergroups/%s.xml" % (self.catalog.service_url,self.name))  
 
     @property
     def layers(self):
         """
         Returns a list of the layers in a layer group
         """
-        url = get_xml("%s/layergroups/%s.xml" % (self.catalog.service_url,self.name))
-        layers = url.find("layers")
+        layers = self.xmlNode.find("layers")
         return [layer.find("name").text for layer in layers]
 
     @property
     def styles(self):
-        return "styles" 
+        styles = self.xmlNode.find("styles")
+        return [style.find("name") for style in styles] 
 
     @property
     def bounds(self):
-        return "bounds"
+        """
+        Make this more useful
+        """
+        bounds = self.xmlNode.find("bounds")
+        minx = bounds.find("minx").text
+        miny = bounds.find("miny").text
+        maxx = bounds.find("maxx").text
+        maxy = bounds.find("maxy").text
+        return [minx,miny,maxx,maxy]
 
     def __str__(self):
         return "<LayerGroup %s>" % self.name

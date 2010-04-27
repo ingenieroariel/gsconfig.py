e51383931ce0e9937adcaec06f05c0f22b0232af
David Winslow <dwinslow@opengeo.org>
2010-04-27 13:30:56 -0400

    Set package name to match project name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "GSConfig",
+setup(name = "gsconfig.py",
     version = "1.0",
     description = "GeoServer REST Configuration Client",
     keywords = "GeoServer REST Configuration",

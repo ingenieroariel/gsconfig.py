958409b8c1553d9a2b19103e1a07626b56fae777
Ariel Núñez <ingenieroariel@gmail.com>
2012-04-12 16:37:38 -0400

    bump to 0.5.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except IOError,e:
     readme_text = ''
 
 setup(name = "gsconfig",
-    version = "0.5.2",
+    version = "0.5.3",
     description = "GeoServer REST Configuration",
     long_description = readme_text,
     keywords = "GeoServer REST Configuration",

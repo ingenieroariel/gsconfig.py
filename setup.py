066fed4ff4ff1e03cbdfd06bbc7a6d330ff35834
Ariel Núñez <ingenieroariel@gmail.com>
2012-04-12 15:49:24 -0400

    Changed name from gsconfig.py to gsconfig and updated metadata

--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@
 
 from setuptools import setup, find_packages
 
-setup(name = "gsconfig.py",
-    version = "1.0",
+setup(name = "gsconfig",
+    version = "0.5",
     description = "GeoServer REST Configuration Client",
     keywords = "GeoServer REST Configuration",
     license = "MIT",
-    url = "http://bitbucket.org/dwins/gsconfigpy",
+    url = "http://github.com/dwins/gsconfig.py",
     author = "David Winslow, Sebastian Benthall",
     author_email = "dwinslow@opengeo.org",
     install_requires = ['httplib2'],
     package_dir = {'':'src'},
     packages = find_packages('src'),
-    test_suite = "test.catalogtests"
+    test_suite = "test.catalogtests",
+    classifiers   = [
+                 'Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Scientific/Engineering :: GIS',
 ) 
 
